704208af521bddf972bb84ed6bfd4c8954059ac5
lum4chi <lum4chi@users.noreply.github.com>
2022-11-15 12:42:56 +0100

    Source code link added

--- Home.py
+++ Home.py
@@ -5,8 +5,11 @@ LAYOUT = "wide"
 INITIAL_SIDEBAR_STATE = "expanded"
 MENU_ITEMS = {
     "About": """
-            Copyright (c) 2022 Presidenza del Consiglio dei Ministri  
-            Datasets are provided [free of charge](https://ec.europa.eu/eurostat/en/about-us/policies/copyright) by © European Union, 1995 - today
+            Code is [open source](https://github.com/teamdigitale/eurostat-datawizard) by Dipartimento della Trasformazione Digitale.  
+            
+            Datasets are provided [free of charge](https://ec.europa.eu/eurostat/en/about-us/policies/copyright) by © European Union, 1995 - today.  
+            
+            Copyright (c) 2022 Presidenza del Consiglio dei Ministri.  
             """
 }
 
