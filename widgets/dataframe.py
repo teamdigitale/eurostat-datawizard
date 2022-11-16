58a05e6534ec4fe6c73925cf0e8b59b37d77d72f
lum4chi <lum4chi@users.noreply.github.com>
2022-11-16 16:32:19 +0100

    Fix + refactored show dataframe (#4)

---
+++ widgets/dataframe.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+
+def st_dataframe_with_index_and_rows_cols_count(dataset: pd.DataFrame, *args, **kwargs):
+    # Dataset is shown with `.reset_index` because MultiIndex are not rendered properly
+    view = dataset if dataset.empty else dataset.reset_index()
+    st.dataframe(view, *args, **kwargs)
+    st.write("{} rows x {} columns".format(*view.shape))
+    return view
