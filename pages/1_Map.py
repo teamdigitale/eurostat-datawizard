053bd24a9c3786d93cb7ee1125c89662586589cf
lum4chi <francesco.lumachi@gmail.com>
2022-11-28 16:17:08 +0100

    Typo

--- pages/1_Map.py
+++ pages/1_Map.py
@@ -11,6 +11,7 @@ from sklearn.manifold import TSNE
 import plotly.express as px
 
 
+@st.experimental_memo
 def build_labeled_toc() -> pd.DataFrame:
     # Datasets starts with a code that identify its theme like:
     # aact_ali01 (Agricultural labour input statistics: absolute...) ->	aact (Economic accounts for agriculture)
@@ -20,32 +21,35 @@ def build_labeled_toc() -> pd.DataFrame:
     a["_join"], b["_join"] = 1, 1
     ab = a.merge(b, on="_join", suffixes=("", "_theme")).drop(columns="_join")
     ab["match"] = ab.apply(lambda x: x["code"].find(x["code_theme"]), axis=1).ge(0)
-    ab = ab[ab.match].drop(columns="match")
+    ab = ab[ab.match].drop(columns=["code_theme", "match"])
     ab = ab.groupby("code").first()
     return ab
 
 
+@st.experimental_memo
 def build_adjacency_matrix() -> pd.DataFrame:
     # Reverse variable index (var -> List(dataset)) can build an adjacency matrix to
     # describe a dataset by the variable used by it.
     codelist = load_codelist_reverse_index()
     adj = pd.get_dummies(codelist.explode())
     adj = adj.groupby("code").any()
-    return adj.T  # Orient datasets as records and variables as features
+    adj = adj.T  # Orient datasets as records and variables as features
+    adj = adj.loc[
+        :, adj.sum() > 1
+    ]  # Remove dataset unique features to speed-up clustering
+    return adj
 
 
 @st.experimental_memo
-def cluster_dataset() -> pd.DataFrame:
-    labeled_toc = build_labeled_toc()
-    adj = build_adjacency_matrix()
-    # Remove dataset unique features to speed-up clustering
-    adj = adj.loc[:, adj.sum() > 1]
+def cluster_datasets(adjacency: pd.DataFrame, toc: pd.DataFrame) -> pd.DataFrame:
     # Project datasets into 2D space
     tsne = TSNE(n_components=2, learning_rate="auto", metric="cosine", init="pca")
-    xy = pd.DataFrame(tsne.fit_transform(adj), index=adj.index, columns=["1st", "2nd"])
+    xy = pd.DataFrame(
+        tsne.fit_transform(adjacency), index=adjacency.index, columns=["1st", "2nd"]
+    )
     xy.index.name = "code"
     # Join datasets with label
-    return xy.join(labeled_toc).reset_index()
+    return xy.join(toc).reset_index()
 
 
 if __name__ == "__main__":
@@ -54,7 +58,9 @@ if __name__ == "__main__":
     st.header("Datasets map")
 
     if get_last_index_update():
-        datasets2d = cluster_dataset()
+        labeled_toc = build_labeled_toc()
+        adj = build_adjacency_matrix()
+        datasets2d = cluster_datasets(adj, labeled_toc)
         margin = 5
         fig = px.scatter(
             datasets2d.rename(columns={"title_theme": "Themes"}),
