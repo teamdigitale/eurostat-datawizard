82754dff913a83d19b96e37e40b15ab61b53119d
lum4chi <francesco.lumachi@gmail.com>
2022-11-28 17:59:16 +0100

    Streamlit Cloud clustering tentative

--- pages/1_Map.py
+++ pages/1_Map.py
@@ -16,15 +16,16 @@ import plotly.express as px
 def build_labeled_toc() -> pd.DataFrame:
     # Datasets starts with a code that identify its theme like:
     # aact_ali01 (Agricultural labour input statistics: absolute...) ->	aact (Economic accounts for agriculture)
-    # Here, a full-join is created and the filter the shorter match to find the generic theme of every dataset.
     toc, themes = load_table_of_contents()
-    a, b = toc.reset_index(), themes.reset_index()
-    a["_join"], b["_join"] = 1, 1
-    ab = a.merge(b, on="_join", suffixes=("", "_theme")).drop(columns="_join")
-    ab["match"] = ab.apply(lambda x: x["code"].find(x["code_theme"]), axis=1).ge(0)
-    ab = ab[ab.match].drop(columns=["code_theme", "match"])
-    ab = ab.groupby("code").first()
-    return ab
+    toc = toc.to_frame()
+    toc["code_theme"] = toc.index.str.split(r"-|_", regex=True).str[0]
+    toc = toc.merge(
+        themes.reset_index().rename(
+            columns={"code": "code_theme", "title": "title_theme"}
+        ),
+        how="left",
+    )
+    return toc
 
 
 @st.experimental_memo
@@ -57,35 +58,35 @@ if __name__ == "__main__":
     app_config("Map")
 
     st.header("Datasets map")
-    if os.environ["ENV"] == "streamlit":
-        st.error(
-            "Datasets clustering is too expensive for streamlit cloud limited resources. You can compute this offline, cloning the repo."
-        )
+    # if os.environ["ENV"] == "streamlit":
+    #     st.error(
+    #         "Datasets clustering is too expensive for streamlit cloud limited resources. You can compute this offline, cloning the repo."
+    #     )
+    # else:
+    if not get_last_index_update():
+        st.warning("Create an index first!")
     else:
-        if not get_last_index_update():
-            st.warning("Create an index first!")
-        else:
-            labeled_toc = build_labeled_toc()
-            adj = build_adjacency_matrix()
-            datasets2d = cluster_datasets(adj, labeled_toc)
-            margin = 5
-            fig = px.scatter(
-                datasets2d.rename(columns={"title_theme": "Themes"}),
-                x="1st",
-                y="2nd",
-                color="Themes",
-                hover_data=["code", "title"],
-                range_x=(
-                    datasets2d["1st"].min() - margin,
-                    datasets2d["1st"].max() + margin,
-                ),
-                range_y=(
-                    datasets2d["2nd"].min() - margin,
-                    datasets2d["2nd"].max() + margin,
-                ),
-                height=1300,
-            )
-            fig.update_layout(legend=dict(orientation="h"))
-            st.plotly_chart(figure_or_data=fig, use_container_width=True)
+        labeled_toc = build_labeled_toc()
+        adj = build_adjacency_matrix()
+        datasets2d = cluster_datasets(adj, labeled_toc)
+        margin = 5
+        fig = px.scatter(
+            datasets2d.rename(columns={"title_theme": "Themes"}),
+            x="1st",
+            y="2nd",
+            color="Themes",
+            hover_data=["code", "title"],
+            range_x=(
+                datasets2d["1st"].min() - margin,
+                datasets2d["1st"].max() + margin,
+            ),
+            range_y=(
+                datasets2d["2nd"].min() - margin,
+                datasets2d["2nd"].max() + margin,
+            ),
+            height=1300,
+        )
+        fig.update_layout(legend=dict(orientation="h"))
+        st.plotly_chart(figure_or_data=fig, use_container_width=True)
 
     show_console()  # For debugging
