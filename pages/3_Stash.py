b9f7c9f605ee2e7d1e3c8063cab95a4171635a6a
lum4chi <francesco.lumachi@gmail.com>
2022-12-06 12:14:52 +0100

    Wide-format added

--- pages/3_Stash.py
+++ pages/3_Stash.py
@@ -46,16 +46,6 @@ def show_stash():
         stash = st.session_state.history
         dataset = empty_eurostat_dataframe()
 
-        try:
-            with st.spinner(text="Fetching data"):
-                dataset = load_stash(stash)
-        except ValueError as ve:
-            st.error(ve)
-
-        view = st_dataframe_with_index_and_rows_cols_count(
-            dataset, "Stash", use_container_width=True
-        )
-
         remove_code = st.sidebar.selectbox(
             "Remove a dataset",
             ["-"] + [code for code, p in stash.items() if p["stash"]],
@@ -64,7 +54,30 @@ def show_stash():
             stash.pop(remove_code)
             st.experimental_rerun()
 
-        download_dataframe_button(view)
+        try:
+            with st.spinner(text="Fetching data"):
+                dataset = load_stash(stash)
+        except ValueError as ve:
+            st.error(ve)
+
+        tab1, tab2 = st.tabs(["Long-format", "Wide-format"])
+        with tab1:
+            view = st_dataframe_with_index_and_rows_cols_count(
+                dataset, use_container_width=True
+            )
+
+            download_dataframe_button(view)
+        with tab2:
+            dataset = dataset.unstack(dataset.index.names.difference(["geo", "time"]))  # type: ignore
+            levels = list(range(len(dataset.columns.names)))
+            dataset = dataset.reorder_levels(
+                levels[1:] + levels[:1], axis=1  # type: ignore
+            ).sort_index(
+                axis=1
+            )  # Move flag, value as last index
+            view = st_dataframe_with_index_and_rows_cols_count(
+                dataset, use_container_width=True  # type: ignore
+            )
     else:
         st.warning("No stash found. Select some data to plot.")
 
