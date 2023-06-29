a3bde6a91b4c90fe9b9bef6b332f059bbaf435ad
lum4chi <francesco.lumachi@gmail.com>
2023-06-29 15:00:18 +0200

    Multiselection refactoring

--- pages/1_🗄️_Data.py
+++ pages/1_🗄️_Data.py
@@ -3,7 +3,14 @@ import logging
 import pandas as pd
 import streamlit as st
 
-from datawizard.data import fetch_table_of_contents
+from datawizard.data import (
+    fetch_table_of_contents,
+    get_cached_session,
+    fetch_metabase,
+    fetch_codelist,
+    parse_codelist,
+    metabase2datasets,
+)
 from st_widgets.commons import (
     app_config,
     get_logger,
@@ -46,97 +53,111 @@ def load_toc() -> pd.Series | None:
         st.sidebar.error(e)
 
 
+@st.cache_data()
+def load_dimension2dataset() -> pd.DataFrame:
+    # Return an index of code + dimension and a list of datasets using them
+    req = get_cached_session()
+    metabase = fetch_metabase(req)
+    codelist = parse_codelist(fetch_codelist(req))
+    return metabase2datasets(metabase, codelist)
+
+
+def build_multiselect_with_selectall_button(options, key: str, label: str):
+    container = st.container()
+
+    if st.button(
+        "Select all",
+        key=f"{key}_all",
+    ):
+        del session[f"{key}_default"]
+        st.experimental_rerun()
+
+    with container:
+        return stateful_multiselect(
+            label=f"{label} ({len(session[key]) if key in session else len(options)}/{len(options)})",
+            options=options,
+            default=options,
+            key=key,
+        )
+
+
 def save_datasets_to_stash():
     toc = load_toc()
 
+    # Datasets search criteria
     if toc is not None:
         with st.sidebar:
-            dataset_code = stateful_selectbox(
-                label="Choose a dataset",
-                options=toc.index,
-                format_func=lambda i: i + " | " + toc.loc[i],
-                key="_selected_dataset",
-            )
-            logging.info(f"Selectbox selection: {dataset_code}")
+            tab1, tab2 = st.tabs(["Select a dataset", "Filter by dimension"])
 
-        if dataset_code is not None:
-            with st.spinner(text="Downloading data"):
-                dataset = load_dataset(dataset_code)
+            with tab1:
+                dataset_code = stateful_selectbox(
+                    label="Choose a dataset",
+                    options=toc.index,
+                    format_func=lambda i: i + " | " + toc.loc[i],
+                    key="_selected_dataset",
+                )
+                logging.info(f"Selectbox selection: {dataset_code}")
 
                 # Create or reuse a filtering history for this code
                 if dataset_code not in session["history"]:
                     session["history"][dataset_code] = dict()
                 history = session["history"][dataset_code]
 
-                history["stash"] = st.sidebar.checkbox(
+                history["stash"] = tab1.checkbox(
                     "Save into Stash",
                     value=history["stash"] if "stash" in history else False,
                 )
 
-                st.subheader(
-                    f"Variable selection: {dataset_code + ' | ' + toc.loc[dataset_code]}"
+            with tab2:
+                # with st.spinner(text="Downloading metadata"):
+                tab2.markdown(
+                    """ 🚧 Work in progress 🚧  
+                    Select only dimensions of interest to filter the dataset list in the previous tab"""
                 )
+                tab2.data_editor(load_dimension2dataset().reset_index())
 
-                # Flags management
-                flags = dataset.flag.fillna("<NA>").unique().tolist()
+        # Dataset filtering criteria
+        if dataset_code is not None:
+            dataset = load_dataset(dataset_code)
 
-                flags_container = st.container()
+            st.subheader(
+                f"Variable selection: {dataset_code + ' | ' + toc.loc[dataset_code]}"
+            )
 
-                if st.button(
-                    "Select all",
-                    key=f"_{dataset_code}.flags_all",
-                ):
-                    del session[f"_{dataset_code}.flags_default"]
-                    st.experimental_rerun()
+            # Flags filtering handles
+            flags = dataset.flag.fillna("<NA>").unique().tolist()
+            history["flags"] = build_multiselect_with_selectall_button(
+                flags, key=f"_{dataset_code}.flags", label="Select FLAG"
+            )
 
-                with flags_container:
-                    history["flags"] = stateful_multiselect(
-                        label="Select FLAG",
-                        options=flags,
-                        default=flags,
-                        key=f"_{dataset_code}.flags",
+            # Indexes filtering handles (all the available dimensions)
+            indexes = {n: dataset.index.levels[i].to_list() for i, n in enumerate(dataset.index.names)}  # type: ignore
+            if "time" in indexes:
+                indexes["time"] = [
+                    min(indexes["time"]).year,
+                    max(indexes["time"]).year,
+                ]
+
+            if "indexes" not in history:
+                history["indexes"] = dict()
+
+            for name in dataset.index.names:
+                if name == "time":
+                    m, M = indexes["time"][0], indexes["time"][1]
+                    M = M if m < M else M + 1  # RangeError fix
+                    history["indexes"]["time"] = stateful_slider(
+                        label="Select TIME [min: 1 year]",
+                        min_value=m,
+                        max_value=M,
+                        value=(m, M),
+                        key=f"_{dataset_code}.indexes.time",
                     )
-
-                # Indexes management
-                indexes = {n: dataset.index.levels[i].to_list() for i, n in enumerate(dataset.index.names)}  # type: ignore
-                if "time" in indexes:
-                    indexes["time"] = [
-                        min(indexes["time"]).year,
-                        max(indexes["time"]).year,
-                    ]
-
-                if "indexes" not in history:
-                    history["indexes"] = dict()
-
-                for name in dataset.index.names:
-                    index_container = st.container()
-
-                    if st.button(
-                        "Select all",
-                        key=f"_{dataset_code}.indexes.{name}_all",
-                    ):
-                        del session[f"_{dataset_code}.indexes.{name}_default"]
-                        st.experimental_rerun()
-
-                    if name == "time":
-                        with index_container:
-                            m, M = indexes["time"][0], indexes["time"][1]
-                            M = M if m < M else M + 1  # RangeError fix
-                            history["indexes"]["time"] = stateful_slider(
-                                label="Select TIME [min: 1 year]",
-                                min_value=m,
-                                max_value=M,
-                                value=(m, M),
-                                key=f"_{dataset_code}.indexes.time",
-                            )
-                    else:
-                        with index_container:
-                            history["indexes"][name] = stateful_multiselect(
-                                label=f"Select {name.upper()}",
-                                options=indexes[name],
-                                default=indexes[name],
-                                key=f"_{dataset_code}.indexes.{name}",
-                            )
+                else:
+                    history["indexes"][name] = build_multiselect_with_selectall_button(
+                        indexes[name],
+                        key=f"_{dataset_code}.indexes.{name}",
+                        label=f"Select {name.upper()}",
+                    )
 
 
 def change_font_size():
